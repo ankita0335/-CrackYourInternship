663e2bd916b1750f58753c6ed667b7ba3a6ec15d
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-08-02 15:24:01 +0530

    Time: 97 ms (40.41%), Space: 41.4 MB (77.48%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0, sum = 0;
+        unordered_map<int, int> sumFreq; // Map to store cumulative sum frequencies
+        sumFreq[0] = 1; // Initialize with a sum of 0
+        
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            
+            // Check if there is a subarray with sum k ending at the current index
+            if (sumFreq.find(sum - k) != sumFreq.end()) {
+                count += sumFreq[sum - k];
+            }
+            
+            // Update the cumulative sum frequency
+            sumFreq[sum]++;
+        }
+        
+        return count;
+    }
+};
