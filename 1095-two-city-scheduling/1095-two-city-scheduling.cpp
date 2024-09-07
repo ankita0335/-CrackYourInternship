c7696a34a436510ca0a00d370af84bc6453a8f63
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-09-07 10:29:02 +0530

    Time: 3 ms (70.56%), Space: 10.6 MB (41.03%) - LeetHub

---
+++ 1095-two-city-scheduling/1095-two-city-scheduling.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), [](const vector<int>&a, const vector<int>&b)
+        {
+            return (a[0]-a[1])< (b[0]-b[1]);
+        });
+        int n=costs.size();
+        int ans=0;
+        for(int i=0;i<n/2;i++)
+        {
+            ans+= costs[i][0];
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            ans+= costs[i][1];
+        }
+        return ans;
+    }
+};
