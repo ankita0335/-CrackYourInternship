2026ca8b137b395d599f8a3d1bd03d60ae757490
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-09-14 23:09:44 +0530

    Time: 59 ms (88.72%), Space: 35.9 MB (52.18%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) 
     {
-        int cnt =0;
+        map<vector<int>, int>mp; // unordered map mai aese vector nhi bn pata hai and iski jo time complexity hogi ordered map ki to search for a row (vector) vo log(n) hogi jbki unordered_map mai find ki t.c. o(1) hoti hai okayy :)
         int n= grid.size();
-        bool f=1;
+        int c=0;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j< n ;j++)
-            {
-                f=1;
-                for(int k=0;k<n;k++)
-                {
-                    if(grid[i][k] != grid[k][j])
-                    {
-                        f=0;
-                        break;
-                    }
-                }
-                cnt += f;
-            }
+            mp[grid[i]]++;
         }
-        return cnt;
+        for(int j=0;j<n;j++)
+        {
+            vector<int>tmp;
+            for(int i=0;i<n;i++)
+                tmp.push_back(grid[i][j]);
+            c += mp[tmp];
+        }
+        return c;
     }
 };
