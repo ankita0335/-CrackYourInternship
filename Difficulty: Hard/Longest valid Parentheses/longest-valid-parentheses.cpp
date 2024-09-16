9c151c2d54bcad0c058e19d2226d2d1bda9e453e
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-09-16 22:33:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxLength(string& str) 
+    {
+        // code here
+        int n= str.length();
+        stack<int>st;
+        st.push(-1);
+        int maxi = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(str[i] == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if(st.empty())
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    maxi= max(maxi, i-st.top());
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.maxLength(S) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
