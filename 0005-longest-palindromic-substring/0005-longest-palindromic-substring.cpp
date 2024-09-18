c8b66dbc6518f11b834a4c25c0f1b11d39e65a60
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-09-18 22:33:21 +0530

    Time: 162 ms (32.82%), Space: 12.8 MB (67.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
+    int dp[1001][1001];
     bool isPal(string &s,int i,int j)
     {
         int n=s.length();
-        while(i<j)
-        {
-            if(s[i]!=s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
+        if(i>=j) return true;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        if(s[i]==s[j])
+            return dp[i][j]=isPal(s, i+1, j-1);
+        return false;
     }
     string longestPalindrome(string s) {
         int n=s.length();
         int maxl=0,st=0;
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<n;i++)
         {
             for(int j=i;j<n;j++)
